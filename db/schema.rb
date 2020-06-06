1ccaf5e2b9f823e6daa0e42a58959676f98e6d52
dodgerty <dodgerty@DESKTOP-LQ6V424.localdomain>
2020-06-05 21:47:19 -0500

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,51 +10,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200402023457) do
+ActiveRecord::Schema.define(version: 2020_04_02_023457) do
 
   create_table "projects", force: :cascade do |t|
-    t.string   "title"
-    t.string   "description"
-    t.integer  "max_group_size"
-    t.integer  "min_group_size"
-    t.string   "topics"
-    t.boolean  "hardware"
-    t.boolean  "industry_sponsored"
-    t.string   "client"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.integer  "section_id"
+    t.string "title"
+    t.string "description"
+    t.integer "max_group_size"
+    t.integer "min_group_size"
+    t.string "topics"
+    t.boolean "hardware"
+    t.boolean "industry_sponsored"
+    t.string "client"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "section_id"
     t.index ["section_id"], name: "index_projects_on_section_id"
   end
 
   create_table "sections", force: :cascade do |t|
-    t.string   "course"
-    t.string   "section"
+    t.string "course"
+    t.string "section"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "students", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "gender"
-    t.string   "ethnicity"
-    t.string   "topics"
-    t.boolean  "hardware"
-    t.string   "electives"
-    t.string   "preferences"
-    t.integer  "section_id"
-    t.integer  "project_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "gender"
+    t.string "ethnicity"
+    t.string "topics"
+    t.boolean "hardware"
+    t.string "electives"
+    t.string "preferences"
+    t.integer "section_id"
+    t.integer "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_students_on_project_id"
     t.index ["section_id"], name: "index_students_on_section_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password"
+    t.string "username"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

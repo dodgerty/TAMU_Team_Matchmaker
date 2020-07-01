a9d9a95593c4193b6545ad0a1d2d960b800f7435
dodgerty <dodgerty@DESKTOP-LQ6V424.localdomain>
2020-06-30 20:52:07 -0500

    p1

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -94,7 +94,55 @@ class ProjectsController < ApplicationController
   end
 
   def match
-    
+    students = Student.where(section_id: params[:section_id])
+    projects = Project.where(section_id: params[:section_id])
+
+    student_scores_hash = {}
+
+    students.each do |student|
+      student_scores_hash[student.id] = {}
+      student_project_ratings = student.preferences.split(',')
+      student_interests = student.topics.split(',')
+      student_electives = student.electives.split(',')
+
+      student_project_ratings.each do |p|
+        preference_parse  = p.split('.')
+        project_id = preference_parse[0].to_i
+        rating  = preference_parse[1].to_i
+        score = 0
+        case rating
+        when 5
+          score = 50
+        when 4
+          score = 30
+        when 3
+          score = 10
+        end
+        project = Project.find(project_id)
+        project_topics = project.topics.split(',')
+        score += (student_interests & project_topics).length()*10
+        elective_map = Student.electiveMap
+        tags = []
+        student_electives.each do |e|
+          tags << elective_map[e].split(',')
+        end
+        tags = tags.flatten.uniq
+        intersecting_electives = tags & project.topics.split(',')
+        score += intersecting_electives.length()*5 
+        student_scores_hash[student.id][project_id] = score
+      end
+
+    end
+    average_score_hash = Hash.new
+    student_scores_hash.each do |student,values|
+      scores = values.values
+      avg = scores.sum/scores.size
+      average_score_hash[student] = avg
+    end
+    average_score_hash = average_score_hash.sort_by{|k, v| v}.to_h
+    puts average_score_hash
+    #puts student_mean_scores.join(' ')
+    print_student_scores(student_scores_hash)
     redirect_to section_projects_path
 
   end
@@ -110,4 +158,10 @@ class ProjectsController < ApplicationController
       params.require(:project).permit(:title, :description, :max_group_size, :min_group_size, :topics, :hardware, :industry_sponsored, :client)
     end
     
+    def print_student_scores(hash)
+      hash.each do |student,project_scores|
+        puts "Student: " + student.to_s
+        puts project_scores
+      end
+    end
 end
